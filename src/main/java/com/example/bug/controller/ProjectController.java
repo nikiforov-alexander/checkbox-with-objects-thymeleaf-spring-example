dd4540086354ddfcfceaf51b8be7491e2ab2c131
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-07-27 09:53:03 +0200

    working with enum controller. added Roles as enum[], `values` to index page

--- src/main/java/com/example/bug/controller/ProjectController.java
+++ src/main/java/com/example/bug/controller/ProjectController.java
@@ -8,22 +8,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class ProjectController {
     @RequestMapping("/")
     public String listRoles(Model model) {
         // create simple list of roles with 2 roles
-        Role role1 = new Role();
-        Role role2 = new Role();
-        role1.setName("role1");
-        role2.setName("role2");
-        List<Role> roles = Arrays.asList(role1, role2);
         // add all roles to model
-        model.addAttribute("allRoles", roles);
+        model.addAttribute("allRoles", Role.values());
         // add empty project to model
         model.addAttribute("project", new Project());
         return "index";
@@ -35,12 +27,6 @@ public class ProjectController {
                              Model model) {
         // print project as it is from thymeleaf
         System.out.println(project);
-        // filter non-null roles
-        List<Role> nonNulRoles =
-                project.getRoles().stream().filter(
-                role -> role.getName() != null).collect(Collectors.toList());
-        // print selected roles: only name is saved
-        System.out.println(nonNulRoles);
         return "redirect:/";
     }
 }
