9a8d0017f0a7895aa1b176fab624f46619231d0e
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-07-27 11:56:01 +0200

    refactored Role, so that it has two fields. Removed `toString()`

--- src/main/java/com/example/bug/model/Role.java
+++ src/main/java/com/example/bug/model/Role.java
@@ -2,21 +2,21 @@ package com.example.bug.model;
 
 
 public class Role {
+
+    private int id;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private String name;
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    String name;
-
-    @Override
-    public String toString() {
-        return "Role{" +
-                "name='" + name + '\'' +
-                '}';
     }
 
     public Role() {
