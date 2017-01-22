256ac27584b1aa2e2576322cad3324835aa91714
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2017-01-22 21:19:46 +0100

    Change old annotation to SpringBootApplication
    It was used by older versions of Spring. Right now simple
    @SpringBootApplication is enough

--- src/main/java/com/example/bug/AppConfig.java
+++ src/main/java/com/example/bug/AppConfig.java
@@ -1,11 +1,9 @@
 package com.example.bug;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class AppConfig {
     public static void main(String[] args) {
         SpringApplication.run(AppConfig.class, args);
