c71fa196229f82497ba3c721c746453fb65dda02
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2017-01-22 21:20:55 +0100

    Rename AppConfig to Application class
    Because it is actually an "application runner" class, not
    config (i.e. not @Configuration class).

--- src/main/java/com/example/bug/Application.java
+++ src/main/java/com/example/bug/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppConfig {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(AppConfig.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
